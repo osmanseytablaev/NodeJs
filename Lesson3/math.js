1e6a4fa77346c0b0a62da0e77ef0bad0b1bc6e2e
osmanseytablaev <osman.seytablaev@gmail.com>
2021-04-06 13:44:46 +0300

    Lesson3 - Create First Module

---
+++ Lesson3/math.js
@@ -0,0 +1,19 @@
+exports.add = function(a, b) {
+    return a + b;
+}
+
+exports.subtract = function(a, b) {
+    return a - b;
+}
+
+exports.multiply = function(a, b) {
+    return a * b;
+}
+/*
+if (command === "add") {
+    console.log(a+b)
+} else if (command === "subtract") {
+    console.log(a-b)
+} else if (command === "multiply") {
+    console.log(a*b)
+}*/
